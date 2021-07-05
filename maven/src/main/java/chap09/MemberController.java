3ca336cc49d338d86da2eeced0e5f3c473cca307
lsm0522 <sysm1230@gmail.com>
2021-07-05 16:41:06 +0900

    커밋

--- maven/src/main/java/chap09/MemberController.java
+++ maven/src/main/java/chap09/MemberController.java
@@ -22,6 +22,7 @@ public class MemberController {
 	
 	@RequestMapping("/member/index.do")
 	public String index(Model model) {
+		System.out.println("이승민");
 		List<MemberVo> list = service.selectList();
 		model.addAttribute("list",list);
 		return "member/index";
